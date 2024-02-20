ac06c6ff0ebcf897895604e135134dda81059dcd
leticiamoreiram <leticia.m@gec.inatel.br>
2024-02-20 16:53:05 -0300

    http methods

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -6,7 +6,25 @@
 // ESModules => import/export
 import http from 'node:http'
 
+// GET => Buscar recurso no back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso no back-end
+
 const server = http.createServer((req, res) => {
+  const {method, url} = req
+
+  if(method === 'GET' && url === '/users') {
+
+    return res.end('Listagem de usuários')
+  }
+
+  if(method === 'POST' && url === '/users') {
+
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Hello World')
 })
 
