c31869ed266ad73aaa76b841f9dcd4a45d2c6b2b
leticiamoreiram <leticia.m@gec.inatel.br>
2024-02-20 17:10:20 -0300

    http code

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -12,20 +12,33 @@ import http from 'node:http'
 // PATCH => Atualizar uma informação específica de um recurso no back-end
 // DELETE => Deletar um recurso no back-end
 
+// Stateful = aplicação depende de informações em memória, dados estão em memória
+// Stateless = os dados se mantêm igual, não salva informações em memória
+
+const users = []
+
 const server = http.createServer((req, res) => {
   const {method, url} = req
 
   if(method === 'GET' && url === '/users') {
 
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if(method === 'POST' && url === '/users') {
 
-    return res.end('Criação de usuário')
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'john@email.com'
+    })
+
+    return res.writeHead(201).end()
   }
 
-  return res.end('Hello World')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
