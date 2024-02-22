c2afef378eb8277242a6a144bfef985984030973
leticiamoreiram <leticia.m@gec.inatel.br>
2024-02-22 19:13:55 -0300

    req json body with stream and buffers

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -17,8 +17,20 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const {method, url} = req
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
 
   if(method === 'GET' && url === '/users') {
 
@@ -29,10 +41,12 @@ const server = http.createServer((req, res) => {
 
   if(method === 'POST' && url === '/users') {
 
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'john@email.com'
+      name,
+      email,
     })
 
     return res.writeHead(201).end()
