f9366025bc82705db345564a8f9de78c0b875b0e
leticiamoreiram <leticia.m@gec.inatel.br>
2024-02-22 19:31:30 -0300

    creating json middleware

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -5,6 +5,7 @@
 
 // ESModules => import/export
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 // GET => Buscar recurso no back-end
 // POST => Criar um recurso no back-end
@@ -20,22 +21,11 @@ const users = []
 const server = http.createServer(async (req, res) => {
   const {method, url} = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if(method === 'GET' && url === '/users') {
 
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
