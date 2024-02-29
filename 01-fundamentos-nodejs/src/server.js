b43dee1390474a2334e3617d9aed0351dc2fad53
leticiamoreiram <leticia.m@gec.inatel.br>
2024-02-29 18:01:22 -0300

    application routes

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -5,9 +5,9 @@
 
 // ESModules => import/export
 import http from 'node:http'
-import { randomUUID } from 'node:crypto' 
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
+
 
 // GET => Buscar recurso no back-end
 // POST => Criar um recurso no back-end
@@ -18,34 +18,17 @@ import { Database } from './database.js'
 // Stateful = aplicação depende de informações em memória, dados estão em memória
 // Stateless = os dados se mantêm igual, não salva informações em memória
 
-const database = new Database()
-
 const server = http.createServer(async (req, res) => {
   const {method, url} = req
 
   await json(req, res)
 
-  if(method === 'GET' && url === '/users') {
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-    const users = database.select('users')
-
-    return res
-      .end(JSON.stringify(users))
-  }
-
-  if(method === 'POST' && url === '/users') {
-
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+  if(route) {
+    return route.hendler(req, res)
   }
 
   return res.writeHead(404).end()
