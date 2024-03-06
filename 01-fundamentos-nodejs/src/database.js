b91b72ae232918386aa1c1dba5a033834d652014
leticiamoreiram <leticia.m@gec.inatel.br>
2024-03-06 17:54:34 -0300

    database list filter

--- 01-fundamentos-nodejs/src/database.js
+++ 01-fundamentos-nodejs/src/database.js
@@ -18,8 +18,17 @@ export class Database{
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table) {
+  select(table, search) {
     const data = this.#database[table] ?? []
+
+    if(search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
+
     return data
   }
 
