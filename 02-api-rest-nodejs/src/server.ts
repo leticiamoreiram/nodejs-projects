350d7a57c15259f2cbcfdab902c0feae2abed325
leticiamoreiram <leticia.m@gec.inatel.br>
2024-03-13 17:45:51 -0300

    enviroment variables

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,13 +1,13 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
+// import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
   const transactions = await knex('transactions')
-  .where('amount', 1000)
-  .select('*')
+    .where('amount', 1000)
+    .select('*')
 
   return transactions
 })
