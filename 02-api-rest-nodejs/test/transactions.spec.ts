6011e8070d809ad179e23d38bfaab3aa50c2d59f
leticiamoreiram <leticia.m@gec.inatel.br>
2024-03-26 16:14:31 -0300

    categorizing tests

---
+++ 02-api-rest-nodejs/test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { it, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/server'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
